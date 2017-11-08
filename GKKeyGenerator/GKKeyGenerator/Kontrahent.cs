0169ba997880297cb199f21e659549fdb9eeb629
GKASZTELANIEC7N\GKasztelaniec <zombi3s@gmail.com>
2017-11-08 23:01:57 +0100

    add working insert customer data into database + draft of select for kontrahent form label2

--- GKKeyGenerator/GKKeyGenerator/Kontrahent.cs
+++ GKKeyGenerator/GKKeyGenerator/Kontrahent.cs
@@ -13,6 +13,7 @@ using System.Data.SqlClient;
 
 namespace GKKeyGenerator
 {
+    
     public partial class Kontrahent : Form
     {
         public Kontrahent()
@@ -44,19 +45,44 @@ namespace GKKeyGenerator
             company.companyAdress = this.textBox3.Text;
             company.companyPCode = this.textBox4.Text;
             company.companyCity = this.textBox5.Text;
+            
 
-            
-            
-            SqlConnection connect = new SqlConnection("Data Source=(local);Initial Catalog=KeyGeneratorTest;Persist Security Info=True;User ID=sa;Password=sa");
-            SqlCommand commInsertCustomer = new SqlCommand();
-            commInsertCustomer.Parameters.AddWithValue("@companyName", company.companyName);
-            commInsertCustomer.Parameters.AddWithValue("@companyNIP", company.companyNIP);
-            commInsertCustomer.Parameters.AddWithValue("@companyAdress", company.companyAdress);
-            commInsertCustomer.Parameters.AddWithValue("@companyPCode", company.companyPCode);
-            commInsertCustomer.Parameters.AddWithValue("@CompanyCity", company.companyCity);
-            
-            commInsertCustomer.CommandText = "insert into dbo.Company (id_company, CompanyName,NIP,Adress,City,PostCode) values (587, @companyName, @companyNIP, @companyAdress, @CompanyCity)";
-            commInsertCustomer.Parameters.Clear();
+            String _connectionString="Data Source=(local);Initial Catalog=KeyGeneratorTest;Persist Security Info=True;User ID=sa;Password=sa";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                String query = "insert into dbo.Company (id_company, CompanyName,NIP,Adress,City,PostCode) values (587, @companyName, @companyNIP, @companyAdress, @CompanyCity, @companyPCode)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@companyName", company.companyName);
+                    command.Parameters.AddWithValue("@companyNIP", company.companyNIP);
+                    command.Parameters.AddWithValue("@companyAdress", company.companyAdress);
+                    command.Parameters.AddWithValue("@companyPCode", company.companyPCode);
+                    command.Parameters.AddWithValue("@CompanyCity", company.companyCity);
+
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                    // Check Error
+                    if (result < 0)
+                        MessageBox.Show("Blad ladowania do bazy!");
+                }
+            }
+         
+             //   using (SqlCommand commSelId = SqlCommand((SELECT MAX(id_company)FROM dbo.Company),connection);
+             //   label2.Text =commSel
+            //listview1 odswierz-refresh
+
+
+
+
+
+
+
+
+
+
         }
 
     }
